76d215675dc90b6c07f196b0685b7f5688bca049
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2022-10-22 16:50:59 -0300

    se arregla tamaño de pantalla

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.SpaceNav;
+//import com.mygdx.game.SpaceNavigation;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("SpaceNav");
+		config.setTitle("SpaceNavigation");
+		config.setWindowedMode(1200, 800);
 		new Lwjgl3Application(new SpaceNav(), config);
 	}
 }
