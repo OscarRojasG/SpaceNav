9eb2671312b318f278cec391afe3a13b55a13862
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-10-26 18:46:02 -0300

    Eliminar imagen de fondo

--- core/src/com/mygdx/game/PantallaMenu.java
+++ core/src/com/mygdx/game/PantallaMenu.java
@@ -8,8 +8,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class PantallaMenu implements Screen {
-	private static final Texture fondo = new Texture(Gdx.files.internal("Fondo.png"));
-	
 	private SpaceNav game;
 	private SpriteBatch batch;
 
@@ -22,7 +20,6 @@ public class PantallaMenu implements Screen {
 	public void render(float delta) {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
 		batch.begin();
-		batch.draw(fondo, 0, 0);
 		
 		game.getFont().draw(batch, "Bienvenido a Space Navigation !", 140, 400);
 		game.getFont().draw(batch, "Presione cualquier tecla o haga click para comenzar ...", 100, 300);
