82cbbf07af02c960f9cd34a9ca49728540cc235f
Lucas <lucasrobles2323@gmail.com>
2022-10-28 11:30:33 -0300

    Comentadas interfaces

--- core/src/com/mygdx/game/Hiriente.java
+++ core/src/com/mygdx/game/Hiriente.java
@@ -1,8 +1,13 @@
 package com.mygdx.game;
 
 public interface Hiriente{
-	
+	/** Clase usada para aplicar un efecto al colicionar con la nave
+	 * @param nave: Recibe atributo de clase Nave para manipularlo
+	 * */
 	public abstract void agregarEfecto(Nave nave);
 	
+	/** Se encarga de enviar el cambio de puntaje que sucede cuando la nave lo colisiona.
+	 * @return int: Puntaje a agregar o quitar en el juego.
+	 * */
 	public int getScoreChange();
 }
--- core/src/com/mygdx/game/Movil.java
+++ core/src/com/mygdx/game/Movil.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
 public interface Movil {
+	
+	/** Se encarga de calcular y actualizar la posicion del objeto en pantalla.*/
 	public void actualizar();
 }
