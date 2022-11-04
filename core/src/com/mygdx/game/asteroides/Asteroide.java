3228b6c002fcd813525e783a331aae3e2c07fcd1
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-11-04 17:50:30 -0300

    tomar una velocidad angular aleatoria

--- core/src/com/mygdx/game/asteroides/Asteroide.java
+++ core/src/com/mygdx/game/asteroides/Asteroide.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Polygon;
 import com.mygdx.game.Enemigo;
 import com.mygdx.game.Nave;
-
+import com.mygdx.game.Util;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
@@ -15,6 +15,10 @@ public class Asteroide extends Enemigo {
 	
     public Asteroide(float x, float y, float ancho, float alto, float velX, float velY, int puntaje) {
     	super(x, y, ancho, alto, velX, velY, puntaje);
+        float r = Util.generateRandomFloat(0, 1);
+        this.getCuerpo().setAngularVelocity(r);
+        System.out.println("R:" + r);
+
     }
 
     
