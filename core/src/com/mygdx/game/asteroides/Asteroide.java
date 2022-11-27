3b3e784fd6d1d716f3351073795954136e4e9b62
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-11-27 16:13:01 -0300

    hacer al asteroide conciente de su tipo

--- core/src/com/mygdx/game/asteroides/Asteroide.java
+++ core/src/com/mygdx/game/asteroides/Asteroide.java
@@ -12,11 +12,14 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class Asteroide extends Enemigo {
+
+    private AsteroideTipo tipo;
 	
-    public Asteroide(float x, float y, float ancho, float alto, float velX, float velY, int puntaje) {
+    public Asteroide(float x, float y, float ancho, float alto, float velX, float velY, AsteroideTipo tipo, int puntaje) {
     	super(x, y, ancho, alto, velX, velY, puntaje);
         float r = Util.generateRandomFloat(0, 1);
         this.getCuerpo().setAngularVelocity(r);
+        this.tipo = tipo;
         System.out.println("R:" + r);
 
     }
