41d1941e8ced8f40cc6c61fb186212a851b35dfa
Lucas <lucasrobles2323@gmail.com>
2022-12-01 17:03:15 -0300

    DisparoNave.java comentada

--- core/src/com/mygdx/game/nave/DisparoNave.java
+++ core/src/com/mygdx/game/nave/DisparoNave.java
@@ -12,6 +12,7 @@ public abstract class DisparoNave {
 		this.nave = nave;
 	}
 	
+	/** @return Bala Es la Bala disparada (si no dispara retorna null) */
     public Bala disparar() {
     	if (puedeDisparar() && !nave.estaHerida()) {
     		reproducirSonidoDisparo();
@@ -20,15 +21,22 @@ public abstract class DisparoNave {
     	return null;
     }
     
+    /** @return Nave Es la nave que dispara */
     public Nave getNave() {
     	return nave;
     }
     
+    /** @return Sound Es el sonido que correspone a disparar */
     public Sound getSonidoDisparo() {
     	return sonidoDisparo;
     }
 	
+    /** @return boolean Es un true si se presiona tecla para disparar y false en caso contrario */
 	public abstract boolean puedeDisparar();
+	
+	/** Reproduce el sonido de disparo con los ajustes correspondientes */
 	public abstract void reproducirSonidoDisparo();
+	
+	/** Genera una Bala respecto a Nave y la retorna */
 	public abstract Bala generarBala();
 }
