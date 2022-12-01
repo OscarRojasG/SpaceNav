13ede0d94d13a6c84bf1daace8629544ea5c3972
Lucas <lucasrobles2323@gmail.com>
2022-12-01 16:05:34 -0300

    Comentadas interaz BalaColisionable y NaveColisionable

--- core/src/com/mygdx/game/BalaColisionable.java
+++ core/src/com/mygdx/game/BalaColisionable.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
 public interface BalaColisionable {
+	/** Aplica acciones correspondientes al colisionar 
+	 * la Bala recibida y el objeto donde aplica el metodo 
+	 * */
 	public void enColisionBala(Bala bala);
 }
--- core/src/com/mygdx/game/NaveColisionable.java
+++ core/src/com/mygdx/game/NaveColisionable.java
@@ -3,5 +3,8 @@ package com.mygdx.game;
 import com.mygdx.game.nave.Nave;
 
 public interface NaveColisionable {
+	/** Aplica acciones correspondientes al colisionar 
+	 * la Nave recibida y el objeto donde aplica el metodo 
+	 * */
 	public void enColisionNave(Nave nave);
 }
